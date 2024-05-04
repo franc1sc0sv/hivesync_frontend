49d3ebebc03954ea88403db95e1076bb77090f54
Almightytgc <crisostomoj63@gmail.com>
2024-05-04 14:38:52 -0600

    Error page modified

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const ErrorMessage: React.FC<Props> = ({ error }) => {
   return (
-    <p className="text-lg text-gray-600">
+    <p className="text-lg text-custom_white">
       {(error as Error)?.message ||
         (error as { statusText?: string })?.statusText}
     </p>
@@ -17,18 +17,16 @@ const Error = () => {
   const error = useRouteError();
   const navigate = useNavigate();
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="p-8 text-center bg-white rounded-lg shadow-lg">
-        <h1 className="text-6xl font-bold text-[#0400DF] mb-2">404</h1>
-        <p className="text-xl text-gray-600">
+    <div className="flex items-center justify-center min-h-screen bg-overlay_1">
+      <div className="p-8 text-center bg-overlay_1 rounded-lg shadow-primary shadow-md">
+        <h1 className="text-6xl font-bold text-custom_white mb-2">404</h1>
+        <p className="text-xl text-custom_white">
           ¡Oops! La página que buscas no se encuentra.
         </p>
-        <p className="text-lg text-gray-600">
-          Parece que has perdido el juego.
-        </p>
+
         <ErrorMessage error={error} />
         <button
-          className="mt-4 bg-[#0400DF] text-white px-4 py-2 rounded-full hover:bg-red-600 transition duration-300"
+          className="mt-4 bg-primary text-white px-4 py-2 rounded-full hover:bg-custom_white hover:text-overlay_1 transition duration-300"
           onClick={() => navigate("/")}
         >
           Volver Atrás
