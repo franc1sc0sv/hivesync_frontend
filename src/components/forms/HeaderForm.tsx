f8d4a1e79f679971d7b1593aea95c0db74a92766
franc1sc0sv <franc1sc0.sv.xd@gmail.com>
2024-05-13 15:14:04 -0600

    index and heather form by Lu

--- src/components/forms/HeaderForm.tsx
+++ src/components/forms/HeaderForm.tsx
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom"
 
 export const HeaderForm = () => {
     return <div>
-        <Link to={"/"} className="text-5xl text-center text-custom_white font-style: font-almarai font-bold">HiveSync</Link>
+        <Link to={"/"} className="text-5xl text-center text-custom_white font-style: font-almarai font-bold">
+            <img className="flex h-12 p-1" src="../public/logo.png"></img>
+        </Link>
         <div className="h-4 bg-primary w-20  rounded-lg mx-auto my-5"></div>
     </div>
 }
--- src/pages/Home/Index/IndexPage.tsx
+++ src/pages/Home/Index/IndexPage.tsx
@@ -7,7 +7,7 @@ export const IndexPage = () => {
         <div className=" flex flex-row p-6 justify-between items-center">
             <img className="flex h-8 p-1" src="../public/logo.png"></img>
             <div className="flex flex-row-reverse gap-3 m-1">
-              <Link className="text-white box-border rounded-sm text-1xl p-2 font-almarai bg-primary" to={"signup"}>Registro</Link>
+              <Link className="text-white box-border rounded-md text-1xl p-2 font-almarai bg-primary" to={"signup"}>Empezar</Link>
               <Link className="text-white text-1xl p-2 font-almarai hover:bg-overlay_2 box-border rounded-sm" to={"/login"}>Iniciar Sesion</Link>
             </div>
           </div>
@@ -19,17 +19,17 @@ export const IndexPage = () => {
             <div className="text-white font-alamari text-sm m-2">
               <p className="z-30">Usa Llamadas, canales de texto, FlashCards, Crea Grupos y explora Diferentes espacios en una interfaz intuitiva de forma gratuita.</p>
             </div>
-            <div className="bg-primary w-16 h-4 box-border rounded-md m-2"><button></button></div>
-            <div className="text-white box-border m-2 rounded-sm bg-primary p-1.5 w-36 flex flex-row justify-between items-center ">
-              <p className="font-anami text-sm m-2 ">Usar HiveSync</p>
+            <div className="bg-primary w-16 h-2 box-border rounded-md m-2"><button></button></div>
+            <div className="text-white box-border rounded-md bg-primary p-1.5 w-max flex gap-4 flex-row justify-between items-center px-5 py-3">
+              <p className="font-amiko font-bold text-sm">Usar HiveSync</p>
               <img className="h-4" src="../public/flecha.png"></img>
             </div>
         </div>
-      </div>
+        </div>
       <div className="p-1 z-0">
-        <img className="w-40 absolute top-40 right-0" src="../public/moneda.png"></img>
-        <img className="w-60 absolute top-60 left-40" src="../public/esfera.png"></img>
-        <img className="w-40 absolute bottom-20 left-80 p-4" src="../public/cono.png"></img>
+        <img className="w-40 absolute top-60 right-0" src="../public/moneda.png"></img>
+        <img className="w-60 absolute top-80 left-40" src="../public/esfera.png"></img>
+        <img className="w-40 absolute top-100 left-80 p-4" src="../public/cono.png"></img>
       </div>
     </main> 
   );
