7098deb97b4da7117840d18bcc89163242df8da7
Almightytgc <crisostomoj63@gmail.com>
2024-05-14 22:31:28 -0600

    Automatic expiration of notifications

--- src/store/useNotifications.ts
+++ src/store/useNotifications.ts
@@ -3,13 +3,19 @@ import { Notifications } from "../types/notifications";
 
 const useNotifications = create<Notifications>((set) => ({
   notifications: [],
-  setNotifications: (notification) =>
+  setNotifications: (notification) => {
     set((state) => ({
       notifications:
         state.notifications.length < 2
           ? [...state.notifications, notification]
           : state.notifications,
-    })),
+    }))
+    setTimeout(() => {
+      set((state) => ({
+        notifications: state.notifications.slice(1)
+      }))
+    }, 5000)
+  },
   removeNotification: (id) =>
     set((state) => ({
       notifications: state.notifications.filter((_, i) => i !== id),
